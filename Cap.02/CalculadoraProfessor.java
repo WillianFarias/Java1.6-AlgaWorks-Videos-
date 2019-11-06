8edd7bec29bf49855129815590c2cb346994a789
williansousafarias@gmail.com <williansousafarias@gmail.com>
2019-11-06 05:53:28 -0300

    Exericio Calculadora de area

---
+++ Cap.02/CalculadoraProfessor.java
@@ -0,0 +1,14 @@
+public class CalculadoraProfessor {
+    public static void main(String args[]){
+        
+        //Constante PI
+        float pi = 3.14f;
+
+        //Informado pelo professor
+        float raio = 5.3f;
+
+        float area = raio * raio * pi;
+
+        System.out.println("Área: " + area);
+    }
+}
