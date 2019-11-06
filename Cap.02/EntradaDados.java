c3ede185407cadfb0e52acaae45ee8ac217365aa
williansousafarias@gmail.com <williansousafarias@gmail.com>
2019-11-06 06:24:17 -0300

    Entrada de dados via terminal

--- Cap.02/EntradaDados.java
+++ Cap.02/EntradaDados.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class EntradaDados {
     public static void main(String [] args){
         
-        Scanner entrada = new Scanner(System.in);
+        Scanner entradaString = new Scanner(System.in);
+        Scanner entradaInt = new Scanner(System.in);
+        Scanner entradaDouble = new Scanner(System.in);
+        
         String nome;
+        int idade;
+        double peso;
 
-        System.out.println("Qual seu nome: ");
-        nome = entrada.nextLine();
+        System.out.print("Qual seu nome: ");
+        nome = entradaString.nextLine();
 
-        System.out.println("Olá " + nome);
+        System.out.print("Qual sua idade: ");
+        idade = entradaInt.nextInt();
+
+        System.out.print("Qual seu peso: ");
+        peso = entradaDouble.nextDouble();
+
+        System.out.println("Olá " + nome +" você tem " + idade + " anos" + " seu peso é " + peso);
     }
 }
