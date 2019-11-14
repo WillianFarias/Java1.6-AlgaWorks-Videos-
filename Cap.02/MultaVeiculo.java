90f459a3801378b54fdd2d71da08e81ea95ebe5a
williansousafarias@gmail.com <williansousafarias@gmail.com>
2019-11-13 21:10:45 -0300

    Exercicio 2.23

---
+++ Cap.02/MultaVeiculo.java
@@ -0,0 +1,47 @@
+/**
+Se o veículo for um carro de passeio e a velocidade do veículo for 10% maior que a velocidade permitida na via, o veículo deve ser multado.
+Se o veículo for um caminhão e a velocidade do veículo for 5% maior que a velocidade permitida na via, o veículo deve ser multado.
+
+Receber as informações de velocidade máxima permitida e a velocidade do veículo
+ */
+
+import java.util.Scanner;
+
+public class MultaVeiculo{
+    public static void main(String[] args){
+
+        int velocidadeMaxima = 0;
+        int velocidadeVeiculo = 0;
+        String tipo = "";
+        Scanner valor = new Scanner(System.in);
+        Scanner valorTexto = new Scanner(System.in);
+
+        System.out.print("Tipo do veículo (caminhão ou passeio): ");
+        tipo = valorTexto.nextLine();
+
+        System.out.print("Velocidade máxima permitida: ");
+        velocidadeMaxima = valor.nextInt();
+
+        System.out.print("Velocidade do veículo: ");
+        velocidadeVeiculo = valor.nextInt();
+
+        //Poderia usar swtich tbm!
+
+        if(tipo.equals("caminhão")){
+            if(velocidadeVeiculo > (velocidadeMaxima + (velocidadeMaxima*0.05))){
+                System.out.println("Veículo multado!");
+            } else {
+                System.out.println("Veiculo não multado!");
+            }
+        } else if (tipo.equals("passeio")){
+            if(velocidadeVeiculo > (velocidadeMaxima + (velocidadeMaxima*0.1))){
+                System.out.println("Veículo multado!");
+            } else {
+                System.out.println("Veiculo não multado!");
+            }
+        } else {
+            System.out.println("Tipo inválido!");
+        }
+
+    }
+}
