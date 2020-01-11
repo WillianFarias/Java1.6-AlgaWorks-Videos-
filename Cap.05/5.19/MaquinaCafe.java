ef7f0be35eb992dca9679be1931cd6e07d65be1b
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-01-11 06:20:08 -0300

    5.19 exer. sobrecarga

---
+++ Cap.05/5.19/MaquinaCafe.java
@@ -0,0 +1,25 @@
+public class MaquinaCafe {
+
+    int acucarDisponivel;
+
+    void fazerCafe() {
+        /*if(acucarDisponivel < 10){
+            System.out.println("Não há açuca suficiente para fazer seu café");
+        } else {
+            acucarDisponivel -= 10;
+            System.out.println("Fazendo cafezinho com 10 gramas de açucar");
+        }*/
+        fazerCafe(10);
+    }
+
+    void fazerCafe(int quantidadeAcucar) {
+        if (acucarDisponivel < quantidadeAcucar){
+            System.out.println("Quantidade disponivel inferior a necessária");
+        } else {
+
+            acucarDisponivel -= quantidadeAcucar;
+            System.out.println("Fazendo cafezinho com " + quantidadeAcucar + " em gramas");
+        }
+    }
+
+}
---
+++ Cap.05/5.19/TesteMaquinaCafe.java
@@ -0,0 +1,7 @@
+class TesteMaquinaCafe {
+    
+    public static void main(String[] args) {
+        MaquinaCafe maquina = new MaquinaCafe();
+        maquina.acucarDisponivel = 30;
+    }
+}
