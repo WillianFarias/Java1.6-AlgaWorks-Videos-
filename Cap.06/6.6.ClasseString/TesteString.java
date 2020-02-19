4df558fd5850cfa0a0a23a6521aea98c93b3dfb6
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-02-19 09:28:27 -0300

    6.6 ClasseString

--- Cap.06/6.6.ClasseString/TesteString.java
+++ Cap.06/6.6.ClasseString/TesteString.java
@@ -1,9 +1,27 @@
 public class TesteString {
   public static void main(String[] args) {
     
-    String s = "Olá";
-    s += " Pessoal!";
+    String s = "Olá";//Cria string "Olá"
+    s += " Pessoal!";//Cria string "Olá Pessoal!" gera desperdicio
+    //de recurso
 
+    StringBuilder sb = new StringBuilder("Olá");//Cria a sb
+    sb.append(" Pesso@l!");//Reaproveita a StringBuilder
+    
+    String resultado = sb.toString();
     System.out.println(s);
+    System.out.println(sb);
+    System.out.println(resultado);
+
+    System.out.println(s.equals(resultado)); 
+
+    if(s == resultado){
+      System.out.println("Ok");
+    }
+
+    //StringBuilder é mais performatico que StringBuffer
+    //No entanto nao é recomendada usar em ambientes com 
+    //multi threads
+    
   }
 }
